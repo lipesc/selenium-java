8fb916658db131932db382c03d05a6524517dde8
lipesc <felipedrhouse@hotmail.com>
2025-12-05 19:53:36 -0300

    tentando o driver manager e removendo a dependencia chrome driver por que é reduntante

--- src/main/java/com/lipesc/App.java
+++ src/main/java/com/lipesc/App.java
@@ -1,13 +1,28 @@
 package com.lipesc;
 
-/**
- * Hello world!
- *
- */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+        WebDriver driver = new ChromeDriver();
+        
+        try {
+           
+            driver.get("https://www.github.com/lipesc");
+            
+            System.out.println("teste de titulo > " + driver.getTitle());
+            
+            Thread.sleep(1000);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
     }
 }
