00c471e35f74dec357bd6dc8c166693cf30530fa
lipesc <felipedrhouse@hotmail.com>
2025-12-06 11:28:43 -0300

    feat: adionado o driver manager

--- src/main/java/com/lipesc/utils/DriverManagerSelenium.java
+++ src/main/java/com/lipesc/utils/DriverManagerSelenium.java
@@ -1,5 +1,25 @@
 package com.lipesc.utils;
 
-public class DriverManagerSelenium {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-}
+public class DriverManagerSelenium {
+    private static WebDriver driver;
+
+       
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+    
+    public static void closerDriver() {
+        if (driver != null) {
+            driver.close();
+            driver = null;
+        }
+    }
+    }
+
